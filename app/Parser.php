88baed55d7487ffe9b57d18b5f66a73ffcac8d25
Kfiros <nevo.kfir@gmail.com>
2026-03-12 18:43:48 +0200

    Implement basic CSV parsing and JSON output generation in `Parser`.

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,23 @@
 
 namespace App;
 
-use Exception;
-
-final class Parser
-{
-    public function parse(string $inputPath, string $outputPath): void
-    {
-        throw new Exception('TODO');
-    }
+final class Parser {
+	public function parse(string $input_path, string $output_path): void {
+		$input = fopen($input_path, 'r');
+		$data = [];
+		while (false !== $visit = fgetcsv($input, escape: '\\')) {
+			[$url, $date] = $visit;
+			$url = substr($url, 19);
+			$date = substr($date, 0, 10);
+			if (!isset($data[$url])) {
+				$data[$url] = [];
+			}
+			if (!isset($data[$url][$date])) {
+				$data[$url][$date] = 0;
+			}
+			$data[$url][$date]++;
+		}
+		fclose($input);
+		file_put_contents($output_path, json_encode($data, JSON_PRETTY_PRINT));
+	}
 }
